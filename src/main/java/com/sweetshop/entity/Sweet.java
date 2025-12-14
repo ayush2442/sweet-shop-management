88762e4efceae7bc30ce4e4ebe0c211494a14d1a
ayush2442 <ayushpandey2442@gmail.com>
2025-12-14 17:36:15 +0530

    entity - user, sweet

---
+++ src/main/java/com/sweetshop/entity/Sweet.java
@@ -0,0 +1,85 @@
+package com.sweetshop.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "sweets")
+public class Sweet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String category;
+
+    @NotNull
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @NotNull
+    @Min(0)
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Sweet() {
+
+    }
+
+    public Sweet(String name, String category, BigDecimal price, Integer quantity) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
